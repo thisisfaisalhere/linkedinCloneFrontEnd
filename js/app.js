003e90ac3ad6c388b69945a4ea4b94f4f58487c7
thisisfaisalhere <thisisfaisalhere@gmail.com>
2020-07-14 13:56:31 +0530

    stop scroll added

--- js/app.js
+++ js/app.js
@@ -5,9 +5,21 @@ const sideMenu = document.querySelector(".navbar-mobile__side");
 menuBtn.addEventListener("click", () => {
     sideMenu.style.display = "block";
     backdrop.style.display = "flex";
+    disableScrolling();
 });
 
 backdrop.addEventListener("click", ()=> {
     sideMenu.style.display = "none";
     backdrop.style.display = "none";
+    enableScrolling();
 });
+
+function disableScrolling() {
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+    window.onscroll=function(){};
+}
