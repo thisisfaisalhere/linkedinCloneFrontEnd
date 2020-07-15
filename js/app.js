22418b06e7b3c13f2f5c875e5e1b4e42ab38b0fc
thisisfaisalhere <thisisfaisalhere@gmail.com>
2020-07-15 10:46:21 +0530

    fixed anim issue

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
 const backdrop = document.querySelector(".backdrop");
 const menuBtn = document.querySelector(".navbar-menu");
 const sideMenu = document.querySelector(".navbar-mobile__side");
+const sideBanner = document.querySelector(".banner-box");
+const menu = document.querySelector(".menu-items");
+const menuItem = document.querySelector(".menu-item");
 
 menuBtn.addEventListener("click", () => {
-    sideMenu.style.display = "block";
+    sideMenu.style.width = "80%";
+    sideMenu.style.visibility = "visible";
+    setTimeout( ()=> {
+        sideBanner.style.display = "flex";
+        menu.style.display = "block";
+    }, 300);
     backdrop.style.display = "flex";
+    menuItem.style.pointerEvents = "all";
     disableScrolling();
 });
 
 backdrop.addEventListener("click", ()=> {
-    sideMenu.style.display = "none";
+    sideMenu.style.width = "0";
+    sideMenu.style.visibility = "hidden";
+    sideBanner.style.display = "none";
+    menu.style.display = "none";
     backdrop.style.display = "none";
+    menuItem.style.pointerEvents = "none";
     enableScrolling();
 });
 
